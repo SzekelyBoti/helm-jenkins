cd8d13b7361b90cb3c330644f2c63714f104970e
SzekelyBoti <szekelyboti1@gmail.com>
2024-12-18 11:29:05 +0100

    Removed self notes/comments

--- helm-app/helm-startup.sh
+++ helm-app/helm-startup.sh
@@ -3,12 +3,9 @@
 # Starting deployment
 echo "Starting deployment"
 
-# Step 1: Install Prometheus using the kube-prometheus-stack chart
 echo "Deploying Prometheus and Grafana"
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f values.yaml
 
-# Step 2: If you have a Helm chart for your Tweet app, deploy it:
 echo "Deploying Tweet application"
 helm install tweet-app ./tweet-app -f values.yaml
-# Step 5: Deployment Complete
 echo "Deployment Complete"
