7f4af27af360071171728c60201e80060077aae5
SzekelyBoti <szekelyboti1@gmail.com>
2025-02-12 10:45:24 +0100

    main.tf added sec-group

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,33 @@ resource "aws_route_table_association" "public_b" {
   route_table_id = aws_route_table.eks_rt.id
 }
 
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Security group for the Application Load Balancer"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Security Group
 resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.eks_vpc.id
