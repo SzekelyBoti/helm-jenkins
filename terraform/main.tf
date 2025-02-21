73e9239d1986a5897ccd798d28774d7cfc8ec5a5
SzekelyBoti <szekelyboti1@gmail.com>
2025-02-21 12:35:37 +0100

    tf tweaks

--- terraform/main.tf
+++ terraform/main.tf
@@ -163,12 +163,18 @@ resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attach" {
   role       = aws_iam_role.node_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cni_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
 # Node Group
 resource "aws_eks_node_group" "node_group" {
   cluster_name  = aws_eks_cluster.my_cluster.name
   node_role_arn = aws_iam_role.node_role.arn
   subnet_ids    = [aws_subnet.eks_subnet_public_a.id, aws_subnet.eks_subnet_public_b.id]
   instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"  # Ensure this uses Amazon EKS optimized AMI
   scaling_config {
     desired_size = 2
     max_size     = 3
